4f1752f64e35bb8f56485d1e00e77656a3858828
NicholasCH <nicholasch96@gmail.com>
2022-03-02 21:20:22 -0600

    first integrations between script and markup

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,15 @@
-$( document).ready( funtion())
+//For Ensuring jQuery is ready; $( document).ready(funtion() {}); Was Short-Handed:
+$(function() {
+    //Variables for conversion from DOM
+    var saver = $(".svBtn");
+    var timeBlock = $(".time-block");
+    //Page Header Day/Time moment.js threads
+    $("#currentDay").text(moment().format('dddd MMMM Do YYYY'));
+    $("#currentTime").text(moment().format('hh:mm'));
+
+    //Color-Coded Hour-Cards indicate Past-hours=WHITE; Future=GREEN; and Current=RED
+
+    function hourIndicate() {
+
+    }
+})
